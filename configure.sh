0c251ce54ea6c8ed7e11420412421c3c5e7e954f
Jonathan Beezley <jonathan.beezley@kitware.com>
2019-04-30 14:19:51 -0400

    Add webhook example

--- configure.sh
+++ configure.sh
@@ -3,5 +3,10 @@
 # These lines need to be run on first run to configure minio.
 mc mb local/test
 mc admin config set local < config.json
-mc admin service restart
+mc admin service restart local
+
+# send all put/delete events to rabbitmq
 mc event add local/test arn:minio:sqs::1:amqp --event put,delete
+
+# send all events with a specific prefix to a webhook for demo purposes
+mc event add local/test arn:minio:sqs::1:webhook --prefix webhook-test/
